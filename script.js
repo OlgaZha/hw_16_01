6c51ff5f617095386f37bf27e3ffee9c7f58a2fa
OlgaZha <zhaldakolga484@gmail.com>
2025-01-18 18:36:00 +0100

    task 2

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 let arr = [32, 23, 65, 4, 8, 10, 12];
 let maxValue = Math.max(...arr);
 console.log(maxValue);
+
+//2.
+let array1 = [1, 0, 2, 3, 4];
+let array2 = [3, 5, 6, 7, 8, 13];
+
+let maxLength = Math.max(array1.length, array2.length);
+
+let arraySum = Array.from({ length: maxLength }, (_, i) => 
+(array1[i] || 0) + (array2[i] || 0));
+
+console.log(arraySum);
